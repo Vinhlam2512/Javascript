44c08fe22536141b630679decb9d0b10242fa764
Vinhlam2512 <lamvinh2431@gmail.com>
2020-12-16 00:18:33 +0700

    1

--- Validator/Validation.js
+++ Validator/Validation.js
@@ -2,6 +2,8 @@
 // Đối tượng Validator
 function Validator(options)  {  //options : object cuả validate
 
+    var selectorRules = {};
+
     // Hàm thực hiện validate
     function validate(inputElement, rule) {
         var errorMessage = rule.test(inputElement.value);     
@@ -22,6 +24,10 @@ function Validator(options)  {  //options : object cuả validate
     if(formElement) { // Nếu có formElement
 
         options.rules.forEach(function (rule) { //Duyệt qua từng rules của object
+
+            // Lưu lại các rules cho mỗi input
+            selectorRules[rule.selector] = rule.test;
+
             
             var inputElement = formElement.querySelector(rule.selector); //rule chọc vào selector của các hàm đã được định nghĩa
             // rule trả về  hàmcủa mảng đã được định nghĩa
@@ -42,6 +48,8 @@ function Validator(options)  {  //options : object cuả validate
                 }
             }
         });
+
+        console.log(selectorRules);
         
     }
 
@@ -51,37 +59,37 @@ function Validator(options)  {  //options : object cuả validate
 // Nguyên tắc của các rules:
 // 1. Khi có lỗi thì trả ra message lỗi
 // 2. Khi hợp lệ thì không trả ra gì (undefined)
-Validator.isRequired = function(selector) { // Selector : fullname
+Validator.isRequired = function(selector, message) { // Selector : fullname
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
         }
     };
 }
 
-Validator.isEmail = function(selector) { // Selector : email
+Validator.isEmail = function(selector, message) { // Selector : email
     return {
         selector: selector,
         test : function (value) {
             var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;// check email
-            return regex.test(value) ? undefined : 'Vui lòng nhập Email';
+            return regex.test(value) ? undefined : message || 'Vui lòng nhập Email';
         }
     };
 }
-Validator.minLength = function(selector, min) { // Selector : password
+Validator.minLength = function(selector, min, message) { // Selector : password
     return {
         selector: selector,
         test: function (value) {
-            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự` ;
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự` ;
         }
     };
 }
-Validator.isConfirmed = function(selector, getConfirmValue) {         // check pass 
+Validator.isConfirmed = function(selector, getConfirmValue, message) {         // check pass 
     return{
         selector: selector,
         test : function (value) {
-            return value === getConfirmValue() ? undefined : 'Giá trị nhập vào không chính xác';
+            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
         }
     }
 }
